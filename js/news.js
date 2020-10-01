7f57e100d8725627c1dfe3265e5d9cd505751951
htmltest <struev.vs@gmail.com>
2020-10-01 16:58:24 +0300

    conference

--- js/news.js
+++ js/news.js
@@ -30,3 +30,28 @@ $(window).on('load resize', function() {
     });
 
 });
+
+$(document).ready(function() {
+
+    $('.conference-section-speakers').each(function() {
+        var curBlock = $(this);
+        if (curBlock.find('.conference-section-speaker').length > 10) {
+            curBlock.find('.conference-section-speakers-more').addClass('visible');
+        }
+    });
+
+    $('.conference-section-speakers-more a').click(function(e) {
+        var curBlock = $(this).parents().filter('.conference-section-speakers');
+        var countItems = curBlock.find('.conference-section-speaker').length;
+        var countVisible = curBlock.find('.conference-section-speaker:visible').length;
+        countVisible += 10;
+        var curTop = curBlock.find('.conference-section-speakers-more').offset().top;
+        if (countVisible >= countItems) {
+            curBlock.find('.conference-section-speakers-more').removeClass('visible');
+        }
+        curBlock.find('.conference-section-speaker:lt(' + countVisible + ')').css({'display': 'inline-block'});
+        $('html, body').animate({'scrollTop': curTop}, 100);
+        e.preventDefault();
+    });
+
+});
